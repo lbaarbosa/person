3de29c7bb6a2db38b21ec9620206798cf2b8ed90
lucasbarbosa <lucas.barbosa@audora.com.br>
2023-01-11 12:50:50 -0300

    feat(entities): Creating entities
    Entities created: person and address

---
+++ src/main/java/api/rest/person/entities/Address.java
@@ -0,0 +1,32 @@
+package api.rest.person.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String logradouro;
+
+    @Column(nullable = false)
+    private String cep;
+
+    @Column(nullable = false)
+    private String numero;
+
+    @Column(nullable = false)
+    private String cidade;
+
+    @Column(nullable = false, unique = true)
+    private boolean mainAddress;
+
+}
---
+++ src/main/java/api/rest/person/entities/Person.java
@@ -0,0 +1,28 @@
+package api.rest.person.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private LocalDate birthDate;
+
+    @OneToMany(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REMOVE})
+    private List<Address> addressList;
+}
